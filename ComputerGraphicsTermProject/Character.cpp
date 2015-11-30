aa826e3d92e893b9460082a78a47d3bc9e2f97a8
WindowsHyun <kwon616@gmail.com>
2015-12-01 01:15:30 +0900

    Update Character Code [ 캐릭터 애니메이션 적용! ] 인호의 도움으로 캐릭턱 애니메이션 다리, 골반, 움직임 설정, 타이머 설정

--- ComputerGraphicsTermProject/Character.cpp
+++ ComputerGraphicsTermProject/Character.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <windows.h>
 
+//텍스쳐 관련 시작
 GLubyte *pBytes; // 데이터를 가리킬 포인터
 BITMAPINFO *info; // 비트맵 헤더 저장할 변수
 GLuint character_head_object[6];
@@ -14,6 +15,14 @@ GLuint character_leg_top_object[6];
 GLuint character_leg_bottom_object[6];
 GLubyte * LoadDIBitmap(const char *filename, BITMAPINFO **info);
 
+void head_Texture();//텍스쳐 적용부분
+void body_Texture();
+void arm_Texture();
+void leg_Texture();
+//텍스쳐 관련 끝
+
+
+//함수선언 시작
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 void Mouse(int, int, int, int);
@@ -21,24 +30,34 @@ void Keyboard(unsigned char, int, int);
 void SpecialKey(int key, int x, int y);
 void TimerFunction(int);
 
-
-void drawBoxFront(int, bool, GLuint);
+void drawBoxFront(int, bool, GLuint);//박스 그리는 함수
 void drawBoxBack(int, bool, GLuint);
 void drawBoxTop(int, bool, GLuint);
 void drawBoxBottom(int, bool, GLuint);
 void drawBoxRight(int, bool, GLuint);
 void drawBoxLeft(int, bool, GLuint);
 
-void head_Texture();
-void body_Texture();
-void arm_Texture();
-void leg_Texture();
-void vLine();
+void vLine();//좌표계 그리는 함수
 
-float fMapZ = 0.0;
+void drawCharacter();
+//함수선언 끝
+
+
+//변수선언 시작
+float fMapZ = 0.0;//화면회전관련 변수
 float vMapX = 0.0;
 float fView = 0.0;
 float vMapY = 0.0;
+int Time = 10;
+
+//애니메이션 변수
+int character_state = 1;//이동 애니메이션 구분용
+float head_angle_x;//머리회전
+float left_sholder_x, left_sholder_y, right_sholder_x, right_sholder_y, left_elbow_x, right_elbow_x;//팔회전
+float left_leg_x, left_leg_y, left_knee_x, right_leg_x, right_leg_y, right_knee_x;//다리회전
+int timer = 0;
+
+//변수선언 끝
 
 
 void main(int argc, char *argv[]){
@@ -49,7 +68,7 @@ void main(int argc, char *argv[]){
 	glutCreateWindow("WindowsHyun - 2012180004"); // 윈도우 생성 (윈도우 이름)
 	glutDisplayFunc(drawScene); // 출력 함수의 지정
 	glutMouseFunc(Mouse);
-	glutTimerFunc(100, TimerFunction, 1);
+	glutTimerFunc(Time, TimerFunction, 1);
 	head_Texture();
 	body_Texture();
 	arm_Texture();
@@ -76,115 +95,8 @@ GLvoid drawScene(GLvoid){
 	//vLine();
 	glPopMatrix();
 
-
-	glPushMatrix(); //Save
-	glTranslated(0, 75, 0);
-	glScaled(1.0, 1.0, 0.7);
-	drawBoxFront(30, false, character_head_object[0]);
-	drawBoxBack(30, false, character_head_object[1]);
-	drawBoxLeft(30, false, character_head_object[2]);
-	drawBoxRight(30, false, character_head_object[3]);
-	drawBoxTop(30, false, character_head_object[4]);
-	drawBoxBottom(30, false, character_head_object[5]);
-	glPopMatrix();
-
-	glPushMatrix(); //Save
-	glScaled(1.0, 1.5, 0.5);
-	drawBoxFront(30, false, character_body_object[0]);
-	drawBoxBack(30, false, character_body_object[1]);
-	drawBoxLeft(30, false, character_body_object[2]);
-	drawBoxRight(30, false, character_body_object[3]);
-	drawBoxTop(30, false, character_body_object[4]);
-	drawBoxBottom(30, false, character_body_object[5]);
-	glPopMatrix();
-
-	glPushMatrix(); //Save right arm Top
-	glTranslated(-45, 44, 0);
-	glRotatef(30, 1, 0, 0);
-	glScaled(0.5, 0.75, 0.5);
-	drawBoxFront(30, true, character_arm_top_object[0]);
-	drawBoxBack(30, true, character_arm_top_object[1]);
-	drawBoxLeft(30, true, character_arm_top_object[2]);
-	drawBoxRight(30, true, character_arm_top_object[3]);
-	drawBoxTop(30, true, character_arm_top_object[4]);
-	drawBoxBottom(30, true, character_arm_top_object[5]);
-	glPushMatrix(); //Save right arm Bottom
-	glTranslated(0, -60, 0);
-	drawBoxFront(30, true, character_arm_bottom_object[0]);
-	drawBoxBack(30, true, character_arm_bottom_object[1]);
-	drawBoxLeft(30, true, character_arm_bottom_object[2]);
-	drawBoxRight(30, true, character_arm_bottom_object[3]);
-	drawBoxTop(30, true, character_arm_top_object[4]);
-	drawBoxBottom(30, true, character_arm_top_object[5]);
-	glPopMatrix();
-	glPopMatrix();
-
-
-	glPushMatrix(); //Save left arm Top
-	glTranslated(45, 44, 0);
-	glRotatef(-30, 1, 0, 0);
-	glScaled(0.5, 0.75, 0.5);
-	drawBoxFront(30, true, character_arm_top_object[0]);
-	drawBoxBack(30, true, character_arm_top_object[1]);
-	drawBoxLeft(30, true, character_arm_top_object[2]);
-	drawBoxRight(30, true, character_arm_top_object[3]);
-	drawBoxTop(30, true, character_arm_top_object[4]);
-	drawBoxBottom(30, true, character_arm_top_object[5]);
-	glPushMatrix(); //Save left arm Bottom
-	glTranslated(0, -60, 0);
-	drawBoxFront(30, true, character_arm_bottom_object[0]);
-	drawBoxBack(30, true, character_arm_bottom_object[1]);
-	drawBoxLeft(30, true, character_arm_bottom_object[2]);
-	drawBoxRight(30, true, character_arm_bottom_object[3]);
-	drawBoxTop(30, true, character_arm_top_object[4]);
-	drawBoxBottom(30, true, character_arm_top_object[5]);
-	glPopMatrix();
-	glPopMatrix();
-
-
-
-	glPushMatrix(); //Save right leg Top
-	glTranslated(-15, -40, 0);
-	glRotatef(-30, 1, 0, 0);
-	glScaled(0.5, 0.75, 0.5);
-	drawBoxFront(30, true, character_leg_top_object[0]);
-	drawBoxBack(30, true, character_leg_top_object[1]);
-	drawBoxLeft(30, true, character_leg_top_object[2]);
-	drawBoxRight(30, true, character_leg_top_object[3]);
-	drawBoxTop(30, true, character_leg_top_object[4]);
-	drawBoxBottom(30, true, character_leg_top_object[5]);
-	glPushMatrix(); //Save right leg Bottom
-	glTranslated(0, -60, 0);
-	drawBoxFront(30, true, character_leg_bottom_object[0]);
-	drawBoxBack(30, true, character_leg_bottom_object[1]);
-	drawBoxLeft(30, true, character_leg_bottom_object[2]);
-	drawBoxRight(30, true, character_leg_bottom_object[3]);
-	drawBoxTop(30, true, character_leg_top_object[4]);
-	drawBoxBottom(30, true, character_leg_top_object[5]);
-	glPopMatrix();
-	glPopMatrix();
-
-
-	glPushMatrix(); //Save left leg Top
-	glTranslated(15, -40, 0);
-	glRotatef(30, 1, 0, 0);
-	glScaled(0.5, 0.75, 0.5);
-	drawBoxFront(30, true, character_leg_top_object[0]);
-	drawBoxBack(30, true, character_leg_top_object[1]);
-	drawBoxLeft(30, true, character_leg_top_object[2]);
-	drawBoxRight(30, true, character_leg_top_object[3]);
-	drawBoxTop(30, true, character_leg_top_object[4]);
-	drawBoxBottom(30, true, character_leg_top_object[5]);
-	glPushMatrix(); //Save left leg Bottom
-	glTranslated(0, -60, 0);
-	drawBoxFront(30, true, character_leg_bottom_object[0]);
-	drawBoxBack(30, true, character_leg_bottom_object[1]);
-	drawBoxLeft(30, true, character_leg_bottom_object[2]);
-	drawBoxRight(30, true, character_leg_bottom_object[3]);
-	drawBoxTop(30, true, character_leg_top_object[4]);
-	drawBoxBottom(30, true, character_leg_top_object[5]);
-	glPopMatrix();
-	glPopMatrix();
+	drawCharacter();
+	
 
 
 	glPopMatrix();
@@ -194,8 +106,52 @@ GLvoid drawScene(GLvoid){
 
 void TimerFunction(int value){
 
+	switch (value){
+	case 1:
+		switch (character_state){
+		case 0://가만히 서있을때
+			left_sholder_x = 0;
+			left_sholder_y = 0;
+			right_sholder_x = 0;
+			right_sholder_y = 0;
+			left_elbow_x = 0;
+			right_elbow_x = 0;
+			left_leg_x = 0;
+			left_leg_y = 0;
+			left_knee_x = 0;
+			right_leg_x = 0;
+			right_leg_y = 0;
+			right_knee_x = 0;
+			break;
+		case 1://전진,후진
+			printf("%d\n", timer);
+			if (timer < 150)
+			{
+				timer++;
+				if (timer < 75)
+				{
+					left_leg_x = timer - 45;
+					right_leg_x = 30 - timer;
+					right_knee_x = timer;
+				}
+				else
+				{
+					left_leg_x = 105 - timer;
+					right_leg_x = timer - 120;
+					left_knee_x = (timer / 2);
+				}
+			}
+			else
+				timer = 0;
+			break;
+		case 2://좌우이동
+			break;
+		}
+		glutTimerFunc(Time, TimerFunction, 1); // 타이머함수 재 설정
+		break;
+	}
+
 	glutPostRedisplay(); // 화면 재 출력
-	glutTimerFunc(100, TimerFunction, 1); // 타이머함수 재 설정
 }
 
 void Keyboard(unsigned char key, int x, int y){
@@ -793,7 +749,144 @@ void leg_Texture(){
 }
 
 
+void drawCharacter()
+{
+	glPushMatrix(); //Save 머리
+	glTranslated(0, 75, 0);
+	glScaled(1.0, 1.0, 0.7);
+	drawBoxFront(30, false, character_head_object[0]);
+	drawBoxBack(30, false, character_head_object[1]);
+	drawBoxLeft(30, false, character_head_object[2]);
+	drawBoxRight(30, false, character_head_object[3]);
+	drawBoxTop(30, false, character_head_object[4]);
+	drawBoxBottom(30, false, character_head_object[5]);
+	glPopMatrix();
 
+	glPushMatrix(); //Save 몸통
+	glScaled(1.0, 1.5, 0.5);
+	drawBoxFront(30, false, character_body_object[0]);
+	drawBoxBack(30, false, character_body_object[1]);
+	drawBoxLeft(30, false, character_body_object[2]);
+	drawBoxRight(30, false, character_body_object[3]);
+	drawBoxTop(30, false, character_body_object[4]);
+	drawBoxBottom(30, false, character_body_object[5]);
+	glPopMatrix();
+
+	glPushMatrix(); //Save 오른팔 어께
+	glTranslated(-45, 44, 0);
+	glRotatef(right_sholder_x, 1, 0, 0);
+	glRotatef(right_sholder_y, 0, 1, 0);
+	glScaled(0.5, 0.75, 0.5);
+	drawBoxFront(30, true, character_arm_top_object[0]);
+	drawBoxBack(30, true, character_arm_top_object[1]);
+	drawBoxLeft(30, true, character_arm_top_object[2]);
+	drawBoxRight(30, true, character_arm_top_object[3]);
+	drawBoxTop(30, true, character_arm_top_object[4]);
+	drawBoxBottom(30, true, character_arm_top_object[5]);
+
+	glPushMatrix(); //Save 오른팔 팔꿈치
+	glTranslated(0, -60, 0);
+	glRotatef(right_elbow_x, 1, 0, 0);
+	drawBoxFront(30, true, character_arm_bottom_object[0]);
+	drawBoxBack(30, true, character_arm_bottom_object[1]);
+	drawBoxLeft(30, true, character_arm_bottom_object[2]);
+	drawBoxRight(30, true, character_arm_bottom_object[3]);
+	drawBoxTop(30, true, character_arm_top_object[4]);
+	drawBoxBottom(30, true, character_arm_top_object[5]);
+	glPopMatrix();//오른팔 팔꿈치 종료
+
+	glPopMatrix();//오른팔 어께 종료
+
+
+	glPushMatrix(); //Save 왼팔 어께
+	glTranslated(45, 44, 0);
+	glRotatef(left_sholder_x, 1, 0, 0);
+	glRotatef(left_sholder_y, 0, 1, 0);
+	glScaled(0.5, 0.75, 0.5);
+	drawBoxFront(30, true, character_arm_top_object[0]);
+	drawBoxBack(30, true, character_arm_top_object[1]);
+	drawBoxLeft(30, true, character_arm_top_object[2]);
+	drawBoxRight(30, true, character_arm_top_object[3]);
+	drawBoxTop(30, true, character_arm_top_object[4]);
+	drawBoxBottom(30, true, character_arm_top_object[5]);
+
+	glPushMatrix(); //Save 왼팔 팔꿈치
+	glTranslated(0, -60, 0);
+	glRotatef(left_elbow_x, 1, 0, 0);
+	drawBoxFront(30, true, character_arm_bottom_object[0]);
+	drawBoxBack(30, true, character_arm_bottom_object[1]);
+	drawBoxLeft(30, true, character_arm_bottom_object[2]);
+	drawBoxRight(30, true, character_arm_bottom_object[3]);
+	drawBoxTop(30, true, character_arm_top_object[4]);
+	drawBoxBottom(30, true, character_arm_top_object[5]);
+	glPopMatrix();//왼팔 팔꿈치 종료
+
+	glPopMatrix();//왼팔어께 종료
+
+
+
+	glPushMatrix(); //Save 오른쪽 골반
+	glTranslated(-15, -40, 0);
+	glRotatef(right_leg_x, 1, 0, 0);
+	glRotatef(right_leg_y, 0, 1, 0);
+	glScaled(0.5, 0.75, 0.5);
+	drawBoxFront(30, true, character_leg_top_object[0]);
+	drawBoxBack(30, true, character_leg_top_object[1]);
+	drawBoxLeft(30, true, character_leg_top_object[2]);
+	drawBoxRight(30, true, character_leg_top_object[3]);
+	drawBoxTop(30, true, character_leg_top_object[4]);
+	drawBoxBottom(30, true, character_leg_top_object[5]);
+	glScaled(2, 1.333333, 2);
+
+	glPushMatrix(); //Save 오른쪽 무릎
+	glTranslated(0, -45, 0);
+	glRotatef(right_knee_x, 1, 0, 0);
+	glScaled(0.5, 0.75, 0.5);
+	drawBoxFront(30, true, character_leg_bottom_object[0]);
+	drawBoxBack(30, true, character_leg_bottom_object[1]);
+	drawBoxLeft(30, true, character_leg_bottom_object[2]);
+	drawBoxRight(30, true, character_leg_bottom_object[3]);
+	drawBoxTop(30, true, character_leg_top_object[4]);
+	drawBoxBottom(30, true, character_leg_top_object[5]);
+	glPopMatrix();//오른쪽 무릎 종료
+
+	glPopMatrix();//오른쪽 골반 종료
+
+
+	glPushMatrix(); //Save 왼쪽 골반
+	glTranslated(15, -40, 0);
+	glRotatef(left_leg_x, 1, 0, 0);
+	glRotatef(left_leg_y, 0, 1, 0);
+	glScaled(0.5, 0.75, 0.5);
+	drawBoxFront(30, true, character_leg_top_object[0]);
+	drawBoxBack(30, true, character_leg_top_object[1]);
+	drawBoxLeft(30, true, character_leg_top_object[2]);
+	drawBoxRight(30, true, character_leg_top_object[3]);
+	drawBoxTop(30, true, character_leg_top_object[4]);
+	drawBoxBottom(30, true, character_leg_top_object[5]);
+	glScaled(2, 1.333333, 2);
+
+	glPushMatrix(); //Save 왼쪽 무릎
+	glTranslated(0, -45, 0);
+	glRotatef(left_knee_x, 1, 0, 0);
+	glScaled(0.5, 0.75, 0.5);
+	drawBoxFront(30, true, character_leg_bottom_object[0]);
+	drawBoxBack(30, true, character_leg_bottom_object[1]);
+	drawBoxLeft(30, true, character_leg_bottom_object[2]);
+	drawBoxRight(30, true, character_leg_bottom_object[3]);
+	drawBoxTop(30, true, character_leg_top_object[4]);
+	drawBoxBottom(30, true, character_leg_top_object[5]);
+	glPopMatrix(); //왼쪽 무릎 종료
+
+	glPopMatrix(); //왼쪽골반 종료
+
+
+
+
+
+	
+
+}
 
 
 
